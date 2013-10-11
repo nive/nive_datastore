54426a83f4801f12547f9a3fe1da576328d0f13b
adroullier <arndt@nive.co>
2013-10-11 18:14:34 +0200

    added python api dispatch

---
+++ nive_datastore/pydispatch.py
@@ -0,0 +1,17 @@
+# Copyright 2013 Arndt Droullier, Nive GmbH. All rights reserved.
+# Released under GPL3. See license.txt
+
+
+__doc__ = """
+Python API Dispatching
+----------------------
+The main Web API is meant to be called through HTTP requests with
+user and state information passed in with the request object. To
+make the same API functionality including security easily useable 
+from python code, this file provides dispatch functions with 
+several options.
+
+
+"""
+
+
